051f798675c42eeca2d5693a3e71a619af1e1369
nihar7540 <nihar7540@gmail.com>
2026-07-15 16:21:32 +0530

    correction

--- 2 Qr code notion link work/largestof3nums.py
+++ 2 Qr code notion link work/largestof3nums.py
@@ -2,7 +2,19 @@ for i in range(5):
     a=int(input("enter first number :"))
     b=int(input("enter secont number:"))
     c=int(input("enter third number :"))
-    if a>b:
+    if a==b:
+        print("a and b are equal")
+        if c>a :
+            print("c is greatest")
+    elif a==c:
+        print("a and c are equal")
+        if b>a :
+            print("c is greatest")
+    elif b==c:
+        print("c and b are equal")
+        if a>b :
+            print("c is greatest")
+    elif a>b:
         if a>c:
             print("a is greatest")
         else:
