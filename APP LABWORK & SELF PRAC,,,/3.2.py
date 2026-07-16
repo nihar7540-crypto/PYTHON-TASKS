d5fe332b78224ed0090566f0be65b48c604d1e07
nihar7540 <nihar7540@gmail.com>
2026-07-16 15:34:54 +0530

    3.3

--- APP LABWORK & SELF PRAC,,,/3.2.py
+++ APP LABWORK & SELF PRAC,,,/3.2.py
@@ -1,102 +1,102 @@
-# for i in range(5):
-#     a=int(input("enter first number :"))
-#     b=int(input("enter secont number:"))
-#     c=int(input("enter third number :"))
-#     if a==b:
-#         print("a and b are equal")
-#         if c>a :
-#             print("a and b are smallest")
-#         else:
-#             print("c is smallest")    
-#     elif a==c:
-#         print("a and c are equal")
-#         if b>a :
-#             print("a and c is smallest")
-#         else:
-#             print("b is smallest")    
-#     elif b==c:
-#         print("c and b are equal")
-#         if a>b :
-#             print("c and b are smallest")
-#         else:
-#             print("a is smallest")    
-#     elif a<b:
-#         if a<c:
-#             print("a is smallest")
-#         else:
-#             print("c is smallest")
-#     elif b<a:
-#         if b<c:
-#             print("b is smallest")
-#         else:
-#             print("c is smallest")
-#     elif a<c:
-#         if a<b:
-#             print("a is smallest")
-#         else:
-#             print("b is smallest")  
-#     else:
-#         print("all are equal") 
+for i in range(5):
+    a=int(input("enter first number :"))
+    b=int(input("enter secont number:"))
+    c=int(input("enter third number :"))
+    if a==b:
+        print("a and b are equal")
+        if c>a :
+            print("a and b are smallest")
+        else:
+            print("c is smallest")    
+    elif a==c:
+        print("a and c are equal")
+        if b>a :
+            print("a and c is smallest")
+        else:
+            print("b is smallest")    
+    elif b==c:
+        print("c and b are equal")
+        if a>b :
+            print("c and b are smallest")
+        else:
+            print("a is smallest")    
+    elif a<b:
+        if a<c:
+            print("a is smallest")
+        else:
+            print("c is smallest")
+    elif b<a:
+        if b<c:
+            print("b is smallest")
+        else:
+            print("c is smallest")
+    elif a<c:
+        if a<b:
+            print("a is smallest")
+        else:
+            print("b is smallest")  
+    else:
+        print("all are equal") 
 
 
-# ##########################################################
+##########################################################
 
-# for i in range(5):
-#     a=int(input("enter first number :"))
-#     b=int(input("enter secont number:"))
-#     c=int(input("enter third number :"))
-#     if a==b:
-#         print("a and b are equal")
-#         if c>a :
-#             print("c is greatest")
-#     elif a==c:                          
-#         print("a and c are equal")
-#         if b>a :
-#             print("c is greatest")
-#     elif b==c:
-#         print("c and b are equal")
-#         if a>b :
-#             print("c is greatest")
-#     elif a>b:
-#         if a>c:
-#             print("a is greatest")
-#         else:
-#             print("c is greatest")
-#     elif b>a:
-#         if b>c:
-#             print("b is greatest")
-#         else:
-#             print("c is greatest")
-#     elif a>c:
-#         if a>b:
-#             print("a is greatest")
-#         else:
-#             print("b is greatest")  
-#     else:
-#         print("all are equal")                  
+for i in range(5):
+    a=int(input("enter first number :"))
+    b=int(input("enter secont number:"))
+    c=int(input("enter third number :"))
+    if a==b:
+        print("a and b are equal")
+        if c>a :
+            print("c is greatest")
+    elif a==c:                          
+        print("a and c are equal")
+        if b>a :
+            print("c is greatest")
+    elif b==c:
+        print("c and b are equal")
+        if a>b :
+            print("c is greatest")
+    elif a>b:
+        if a>c:
+            print("a is greatest")
+        else:
+            print("c is greatest")
+    elif b>a:
+        if b>c:
+            print("b is greatest")
+        else:
+            print("c is greatest")
+    elif a>c:
+        if a>b:
+            print("a is greatest")
+        else:
+            print("b is greatest")  
+    else:
+        print("all are equal")                  
 
-# #####################################################
+#####################################################
 
 
 
-# num1 = float(input("Enter first number: "))
-# operator = input("Enter operator (+, -, *, /): ")
-# num2 = float(input("Enter second number: "))
+num1 = float(input("Enter first number: "))
+operator = input("Enter operator (+, -, *, /): ")
+num2 = float(input("Enter second number: "))
 
-# match operator:
-#     case "+":
-#         print("Result:", num1 + num2)
-#     case "-":
-#         print("Result:", num1 - num2)
-#     case "*":
-#         print("Result:", num1 * num2)
-#     case "/":
-#         if num2 != 0:
-#             print("Result:", num1 / num2)
-#         else:
-#             print("Error: Division by zero")
-#     case _:
-#         print("Error: Invalid operator")
+match operator:
+    case "+":
+        print("Result:", num1 + num2)
+    case "-":
+        print("Result:", num1 - num2)
+    case "*":
+        print("Result:", num1 * num2)
+    case "/":
+        if num2 != 0:
+            print("Result:", num1 / num2)
+        else:
+            print("Error: Division by zero")
+    case _:
+        print("Error: Invalid operator")
 
 ######################################################
 order=int(input("enter what do you want to oder 1.sandwhich 2.pizza 3.burger  :"))
@@ -130,3 +130,51 @@ match order:
                 print("ordered restro special burger")    
     case _:
         print("invalid order")        
+
+################################################################
+
+def telecom_system():
+    print("--- Telecom Calling System ---")
+    print("1. English")
+    print("2. Hindi")
+    print("3. Gujarati")
+    
+    choice = input("Please select your language (1/2/3): ")
+
+    match choice:
+        case '1':
+            print("\nYou selected English.")
+            print("1. Account Balance")
+            print("2. Talk to Customer Care")
+            sub_choice = input("Select an option (1/2): ")
+            
+            match sub_choice:
+                case '1': print("Your balance is $50.00.")
+                case '2': print("Connecting you to English support...")
+                case _: print("Invalid selection.")
+
+        case '2':
+            print("\nआपने हिंदी चुनी है।")
+            print("1. खाता शेष")
+            print("2. ग्राहक सेवा से बात करें")
+            sub_choice = input("विकल्प चुनें (1/2): ")
+            
+            match sub_choice:
+                case '1': print("आपका बैलेंस 50 रुपये है।")
+                case '2': print("हिंदी सहायता से जोड़ा जा रहा है...")
+                case _: print("अमान्य चयन।")
+
+        case '3':
+            print("\nતમે ગુજરાતી પસંદ કર્યું છે.")
+            print("1. એકાઉન્ટ બેલેન્સ")
+            print("2. કસ્ટમર કેર સાથે વાત કરો")
+            sub_choice = input("વિકલ્પ પસંદ કરો (1/2): ")
+            
+            match sub_choice:
+                case '1': print("તમારું બેલેન્સ 50 રૂપિયા છે.")
+                case '2': print("ગુજરાતી સપોર્ટ સાથે જોડી રહ્યા છીએ...")
+                case _: print("અમાન્ય પસંદગી.")
+
+        case _:
+            print("\nInvalid language selection.")
+telecom_system()
--- APP LABWORK & SELF PRAC,,,/3.3.py
+++ APP LABWORK & SELF PRAC,,,/3.3.py
@@ -1,1 +1,47 @@
-# Empty file
+# from _typeshed import importlib
+# from _typeshed import importlib
+# from _typeshed import importlib
+# while True:
+#     num=int(input("enter number: "))
+#     if num==0:
+#         break
+
+# #################################################
+# i=1
+# while i<=50:
+#     if i%2==0:
+#         print(i)
+#     i+=1
+
+# ##################################################
+
+# for i in range(1,20+1):
+#     if i%2!=0:
+#         print(i)
+
+# ##################################################
+
+# for i in range(5,50+1,5):
+#     print(i)
+
+# #################################################
+
+# for i in range(10,0,-1):
+#     print(i)
+
+# #################################################
+
+# for i in range(1,10+1):
+#     sq=i**2
+#     print(sq)
+
+# #################################################
+
+for i in range (1,50+1):
+    if i%3==0 and i%2!=0:
+        print(i,"divisible by 3 only")
+    elif i%2==0 and i%3!=0:
+        print(i,"divisible by 2 only")
+    else:
+        print(i,"divisible by neither 2 nor 3")
+        
